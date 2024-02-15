627a5c8a486279da7ef1c1c54bc5e45647990c71
Karan V. Kushwaha <64746933+Karan1002@users.noreply.github.com>
2024-02-15 16:55:39 +0530

    Update uploadConfig.js

--- config/uploadConfig.js
+++ config/uploadConfig.js
@@ -1,23 +1,14 @@
 // const cloudinary = require("cloudinary").v2;
 require("dotenv").config();
 
-// cloudinary.config({
-//   // cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   // api_key: process.env.CLOUDINARY_API_KEY,
-//   // api_secret: process.env.CLOUDINARY_API_SECRET,
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
-
-const cloudinary = require("cloudinary");
-
-cloudinary.v2.config({
-  cloud_name: "damq4ghel",
-  api_key: "366593616415934",
-  api_secret: "9DBxZEXzZqTk2saZCKvIozCQGV8",
-  secure: true,
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+
 
 const uploadFileConfig = async (filePath) => {
   console.log(filePath);
